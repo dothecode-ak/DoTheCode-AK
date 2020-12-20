c66976161d516164a317acb8a6b3086df110c35b
Anil Yadav <iter.anilyadav@gmail.com>
2020-12-20 20:21:11 +0530

    1 st pattern logic

--- pattern/1.js
+++ pattern/1.js
@@ -14,3 +14,26 @@ for (var i = 0; i < 5; i++) {
 
 }
 
+//pattern 2
+//1 1 1 1 1
+//2 2 2 2
+//3 3 3
+//4 4
+//5
+
+for (var k = 1; k <= 5; k++) {
+    for (var m = k; m <= 5; m++) {
+        document.write(k);
+    }
+    document.write("<br/>")
+}
+
+
+//pattern 3
+
+for (n = 1; n <= 5; n++) {
+    for (var z = n; z <= 5; z++) {
+        document.write(z);
+    }
+    document.write("<br/>")
+}
