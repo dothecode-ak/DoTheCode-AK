e9be591dd4f9b672adbbf6dbbd25f12ef2b896db
Anil Yadav <iter.anilyadav@gmail.com>
2020-12-20 20:12:51 +0530

    1 st pattern logic

---
+++ pattern/1.js
@@ -0,0 +1,16 @@
+//logic
+// print the pattern
+// * * * * *
+// * * * * 
+// * * * 
+// * * 
+// * 
+
+for (var i = 0; i < 5; i++) {
+    for (var j = i; j < 5; j++) {
+        document.write("*");
+    }
+    document.write('<br/>')
+
+}
+
