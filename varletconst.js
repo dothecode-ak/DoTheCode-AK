a9a9843b353a797c9ed874e506d8b4bd9b48406d
Anil Yadav <iter.anilyadav@gmail.com>
2020-12-24 12:06:30 +0530

    suffle js

---
+++ varletconst.js
@@ -0,0 +1,20 @@
+//pre decrement prefix
+let x = 3;
+const y = --x;
+console.log(x, y)//2 2
+
+//post decrement prefix
+
+let a = 10;
+b = a--;
+console.log(a, b)//9 10
+
+//pre increment prefix
+let m = 9;
+n = ++m;
+console.log(m, n);//10 10
+
+//post increment prefix
+let p = 9;
+q = p++;
+console.log(p, q);//10 9
