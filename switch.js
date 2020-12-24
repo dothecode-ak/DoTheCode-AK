9ab1edf019c9bebca12480f8e6f9ba2a5a2e9599
Anil Yadav <iter.anilyadav@gmail.com>
2020-12-24 12:13:14 +0530

    switch learn

---
+++ switch.js
@@ -0,0 +1,32 @@
+var token = 5;
+switch (token) {
+    case 1:
+        console.log("1 logic is implemented");
+        break;
+    case 2:
+        console.log("2 logic is implemented");
+        break;
+    case 3:
+        console.log("3 logic is implemented");
+        break;
+    case 4:
+        console.log("4 logic is implemented");
+        break;
+    case 5:
+        console.log("5 logic is implemented");
+        break;
+    case 6:
+        console.log("6 logic is implemented");
+        break;
+    case 7:
+        console.log("7 logic is implemented");
+        break;
+    case 8:
+        console.log("8 logic is implemented");
+        break;
+    case 9:
+        console.log("9 logic is implemented");
+        break;
+    default:
+        console.log("Not found");
+}
