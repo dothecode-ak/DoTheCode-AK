645beb2be051b6f99caf8df09cbba7d4e0c1a3e1
Anil Yadav <iter.anilyadav@gmail.com>
2021-01-25 14:51:50 +0530

    ES 7/8

---
+++ ES-8/padStart.js
@@ -0,0 +1,6 @@
+let padStartString = '1600'.padStart(7, 0);
+console.log(padStartString);
+console.log(padStartString.length)
+let padStartString2 = '1600'.padStart(7);
+console.log(padStartString2);
+console.log(padStartString2.length)
