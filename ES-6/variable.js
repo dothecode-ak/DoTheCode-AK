8657aaafa57732aa7171453dc922fbb3a159ef27
anil-nodejs <sde.anilyadav@gmail.com>
2021-09-07 15:26:22 +0530

    ES6 variable scope

---
+++ ES-6/variable.js
@@ -0,0 +1,19 @@
+'use-strict'
+//let
+if(true)
+{
+    let a=1;
+    console.log(a);
+}
+//var
+var data='Anil';
+const tfunc=(t1)=>
+{
+    var age=28;
+console.log(t1+' '+age)
+}
+tfunc(data);
+//const 
+const t2=10;
+// t2=10;
+// console.log(t2)
